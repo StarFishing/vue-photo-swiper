a35f95f449b2c49e3e39c4c5ac0ab174513ab74f
xiaoxin <2923627211@qq.com>
2020-04-02 20:00:50 +0800

    change component plugin way

--- src/components/PhotoSwiper/index.ts
+++ src/components/PhotoSwiper/index.ts
@@ -1,6 +1,6 @@
 import PhotoSwiper from './index.vue'
-let components = PhotoSwiper as any
-components.install = function(Vue: any) {
+// @ts-ignore
+PhotoSwiper.install = function(Vue: any) {
   Vue.component(PhotoSwiper.name, PhotoSwiper)
 }
 
--- src/components/index.ts
+++ src/components/index.ts
@@ -19,6 +19,5 @@ if (typeof window !== 'undefined' && (window as any).Vue) {
 export default {
   // 导出的对象必须具有 install，才能被 Vue.use() 方法安装
   install,
-  // 以下是具体的组件列表
-  PhotoSwiper,
 }
+export { PhotoSwiper }
